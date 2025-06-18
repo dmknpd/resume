ef8792d97ca5b7ab09fc0550eb99ba540c7639af
dmknpd <soupuabt@gmail.com>
2025-06-18 12:35:28 +0300

    added techs

--- src/components/StackList/StackList.js
+++ src/components/StackList/StackList.js
@@ -6,8 +6,11 @@ import css from "../../assets/img/css.svg";
 import scss from "../../assets/img/scss.svg";
 import js from "../../assets/img/js.svg";
 import react from "../../assets/img/react.svg";
+import redux from "../../assets/img/redux.svg";
 import vue from "../../assets/img/vue.svg";
+import pinia from "../../assets/img/pinia.svg";
 import tailwindcss from "../../assets/img/tailwindcss.svg";
+import bootstrap from "../../assets/img/bootstrap.svg";
 import node_js from "../../assets/img/node_js.svg";
 import express from "../../assets/img/express.svg";
 import php from "../../assets/img/php.svg";
@@ -20,36 +23,80 @@ import mongodb from "../../assets/img/mongodb.svg";
 import git from "../../assets/img/git.svg";
 import docker from "../../assets/img/docker.svg";
 import figma from "../../assets/img/figma.svg";
+import npm from "../../assets/img/npm.svg";
+import webpack from "../../assets/img/webpack.svg";
+import gulp from "../../assets/img/gulp.svg";
+import postman from "../../assets/img/postman.svg";
 
 const StackList = () => {
-  const techs = [
+  const front = [
     { id: 1, title: "HTML", icon: html },
     { id: 2, title: "CSS", icon: css },
     { id: 3, title: "SCSS", icon: scss },
     { id: 4, title: "JavaScript", icon: js },
     { id: 5, title: "React", icon: react },
-    { id: 6, title: "Vue", icon: vue },
-    { id: 7, title: "TailwindCSS", icon: tailwindcss },
-    { id: 8, title: "Node.js", icon: node_js },
-    { id: 9, title: "Express.js", icon: express },
-    { id: 10, title: "PHP", icon: php },
-    { id: 11, title: "Laravel", icon: laravel },
-    { id: 12, title: "RESTful API", icon: rest_api },
-    { id: 13, title: "WebSocket", icon: websocket },
-    { id: 14, title: "MySQL", icon: mysql },
-    { id: 15, title: "PostgreSQL", icon: postgresql },
-    { id: 16, title: "MongoDB", icon: mongodb },
-    { id: 17, title: "Git", icon: git },
-    { id: 18, title: "Docker", icon: docker },
-    { id: 19, title: "Figma", icon: figma },
+    { id: 6, title: "Redux", icon: redux },
+    { id: 7, title: "Vue", icon: vue },
+    { id: 8, title: "Pinia", icon: pinia },
+    { id: 9, title: "Bootstrap", icon: bootstrap },
+    { id: 10, title: "TailwindCSS", icon: tailwindcss },
+  ];
+
+  const back = [
+    { id: 11, title: "Node.js", icon: node_js },
+    { id: 12, title: "Express.js", icon: express },
+    { id: 13, title: "PHP", icon: php },
+    { id: 14, title: "Laravel", icon: laravel },
+    { id: 15, title: "RESTful API", icon: rest_api },
+    { id: 16, title: "WebSocket", icon: websocket },
+  ];
+
+  const db = [
+    { id: 17, title: "MySQL", icon: mysql },
+    { id: 18, title: "PostgreSQL", icon: postgresql },
+    { id: 19, title: "MongoDB", icon: mongodb },
+  ];
+
+  const tools = [
+    { id: 20, title: "Git", icon: git },
+    { id: 21, title: "npm", icon: npm },
+    { id: 22, title: "Postman", icon: postman },
+    { id: 23, title: "Docker", icon: docker },
+    { id: 24, title: "Webpack", icon: webpack },
+    { id: 25, title: "Gulp", icon: gulp },
+    { id: 26, title: "Figma", icon: figma },
   ];
 
   return (
-    <div className={styles.stack_list}>
-      {techs.map((tech) => (
-        <StackItem key={tech.id} title={tech.title} icon={tech.icon} />
-      ))}
-    </div>
+    <>
+      <f3 className={styles.title}>Front-end</f3>
+      <div className={styles.stack_list}>
+        {front.map((tech) => (
+          <StackItem key={tech.id} title={tech.title} icon={tech.icon} />
+        ))}
+      </div>
+
+      <f3 className={styles.title}>Back-end</f3>
+      <div className={styles.stack_list}>
+        {back.map((tech) => (
+          <StackItem key={tech.id} title={tech.title} icon={tech.icon} />
+        ))}
+      </div>
+
+      <f3 className={styles.title}>Databases</f3>
+      <div className={styles.stack_list}>
+        {db.map((tech) => (
+          <StackItem key={tech.id} title={tech.title} icon={tech.icon} />
+        ))}
+      </div>
+
+      <f3 className={styles.title}>Tools</f3>
+      <div className={styles.stack_list}>
+        {tools.map((tech) => (
+          <StackItem key={tech.id} title={tech.title} icon={tech.icon} />
+        ))}
+      </div>
+    </>
   );
 };
 
